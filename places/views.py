411f34511e674e3fe55b09ed5184995d367458fc
aqwarius2003 <00victorr@gmail.com>
2024-11-29 19:22:15 +0400

    Created the Endpoint API

--- places/views.py
+++ places/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Place
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -28,4 +29,25 @@ def index(request):
 
     return render(request, 'index.html', context={
         'places': places_data
+    })
+
+
+def place_detail(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    place_detail= {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [place.latitude]
+        },
+        "properties": {
+            "title": place.title,
+            "description_short": place.description_short,
+            "description_long": place.description_long,
+            "placeId": place.title,
+            "detailsUrl": "static/places/roofs24.json"
+        }
+    }
+    return render(request, 'place_detail.html', context={
+        'place': place_detail
     })
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -9,4 +9,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('places/<int:place_id>/', views.place_detail, name='place_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
