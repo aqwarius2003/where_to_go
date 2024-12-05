3a2bad6d890044dc799ad58225d87817250dbf0c
aqwarius2003 <00victorr@gmail.com>
2024-12-05 23:54:56 +0400

    Added a WYSIWYG editor

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from places import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,4 +8,5 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', views.index, name='index'),
                   path('place/<int:place_id>/', views.place_detail, name='place'),
+                  path('tinymce/', include('tinymce.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
