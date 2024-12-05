e9aee12c6c563435d157fe0ce36c06b69a7d54b8
aqwarius2003 <00victorr@gmail.com>
2024-12-05 23:03:33 +0400

    Added photo previews

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from .models import Place, Photo
+from django.utils.html import format_html
 
+def generate_image_preview(obj):
+    if hasattr(obj, 'img') and obj.img:
+        return format_html('<img src="{}" style="height: 100px;" />', obj.img.url)
+    return "No Image"
 
 class PlaceImageInline(admin.TabularInline):
     model = Photo
+    extra = 0
+    verbose_name = 'Фото'
+    verbose_name_plural = 'Фотографии'
+    fields = ('img', 'order', 'image_preview')
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        return generate_image_preview(obj)
+
+    image_preview.short_description = 'Превью'
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
@@ -12,9 +28,15 @@ class PlaceAdmin(admin.ModelAdmin):
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('place', 'order')
+    fields = ('place', 'order', 'img', 'image_preview')
+    readonly_fields = ('image_preview',)
+    list_display = ('place', 'order', 'image_preview')
     list_editable = ('order',)
     ordering = ('place', 'order')
 
-    def place(self, obj):
-        return obj.place.title
+    def image_preview(self, obj):
+        return generate_image_preview(obj)
+
+    image_preview.short_description = 'Превью'
+
+
