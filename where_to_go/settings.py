40bf4f08566fe745e10965625d65b8b4d4c3c2a1
aqwarius2003 <00victorr@gmail.com>
2024-12-09 17:20:06 +0400

    Separated STATIC_ROOT from STATICFILES_DIRS, PEP8

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -111,12 +111,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Папка для хранения статических файлов во время разработки:
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'static/media'
-# STATIC_ROOT = BASE_DIR / 'static'
+# Папка для хранения статических файлов во время разработки:
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
