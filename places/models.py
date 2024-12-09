f5edb8d7487500ffb7c7e4b297e59e2b6b835939
aqwarius2003 <00victorr@gmail.com>
2024-12-09 17:27:07 +0400

    I removed the extra hint

--- places/models.py
+++ places/models.py
@@ -6,8 +6,8 @@ class Place(models.Model):
     title = models.CharField(max_length=255, unique=True, verbose_name='Локация')
     description_short = models.TextField(verbose_name='Краткое описание', blank=True, null=True)
     description_long = HTMLField(verbose_name='Детальное описание', blank=True, null=True)
-    latitude = models.FloatField(verbose_name='Широта (55)', null=True)  # 55
-    longitude = models.FloatField(verbose_name='Долгота (37)', null=True)  # 37
+    latitude = models.FloatField(verbose_name='Широта', null=True)
+    longitude = models.FloatField(verbose_name='Долгота', null=True)
 
     def __str__(self):
         return self.title
