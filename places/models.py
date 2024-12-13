a1a6ac5ca1e61c81fcd2ee4e6aa18cb68dad63f4
aqwarius2003 <00victorr@gmail.com>
2024-12-13 17:51:36 +0400

    Edited manage.py

--- places/models.py
+++ places/models.py
@@ -4,27 +4,27 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=255, unique=True, verbose_name='Локация')
-    description_short = models.TextField(verbose_name='Краткое описание', blank=True, null=True)
-    description_long = HTMLField(verbose_name='Детальное описание', blank=True, null=True)
-    latitude = models.FloatField(verbose_name='Широта', null=True)
-    longitude = models.FloatField(verbose_name='Долгота', null=True)
-
-    def __str__(self):
-        return self.title
+    short_description = models.TextField(verbose_name='Краткое описание', blank=True)
+    long_description = HTMLField(verbose_name='Детальное описание', blank=True)
+    latitude = models.FloatField(verbose_name='Широта')
+    longitude = models.FloatField(verbose_name='Долгота')
 
     class Meta:
         verbose_name = 'Место'
         verbose_name_plural = 'Места'
         ordering = ['title']
 
+    def __str__(self):
+        return self.title
+
 
 class Photo(models.Model):
-    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='photos')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='photos', verbose_name='Локация')
     img = models.ImageField(verbose_name='Фото')
-    order = models.IntegerField(default=0, verbose_name='Порядковый номер')
-
-    def __str__(self):
-        return f'{self.order}'
+    order = models.IntegerField(default=0, verbose_name='Порядковый номер', db_index=True)
 
     class Meta:
         ordering = ['order']
+
+    def __str__(self):
+        return f'{self.order}'
